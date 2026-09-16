2c75e34d2d8bfcd93f6d11d006fbe4e0b0b37898
pseudoical <pseudoical@gmail.com>
2026-09-16 15:35:05 -0700

    perf: enable GPU acceleration flags

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { app, BrowserWindow, globalShortcut } from "electron";
 
 
+// navigator.userAgent: Chrome/152.0.7977.78 Electron/44.4.1
+// https://peter.sh/experiments/chromium-command-line-switches/
+app.commandLine.appendSwitch("use-vulkan");
+app.commandLine.appendSwitch("enable-features", "Vulkan");
+app.commandLine.appendSwitch("enable-zero-copy");
+app.commandLine.appendSwitch("ignore-gpu-blocklist");
+app.commandLine.appendSwitch("show-fps-counter");
+
 app.whenReady().then(() => {
     const win = new BrowserWindow({
         width: 800,
