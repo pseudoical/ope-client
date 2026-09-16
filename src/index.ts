84c2c0f1013d7ec89278194f43f0ebce484f582d
pseudoical <pseudoical@gmail.com>
2026-09-16 15:12:05 -0700

    feat: add browser window

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, Apes.io!");
+import { app, BrowserWindow } from "electron";
+
+app.whenReady().then(() => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        backgroundColor: "#000",
+    });
+
+    win.menuBarVisible = false;
+    win.loadURL("https://apes.io/test");
+
+    // Ignore the page's exit confirmation prompt.
+    win.webContents.on("will-prevent-unload", (event) => {
+        event.preventDefault();
+    });
+});
